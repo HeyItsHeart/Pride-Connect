9a8a89c6da2cb7b57efc608534e13808f755b0b6
HeyItsHeart <townerlucas@gmail.com>
2026-02-23 13:44:30 -0600

    Create service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = "prideconnect-cache-v1";
+const URLS_TO_CACHE = ["/", "/offline.html", "/manifest.json"];
+
+// Install
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+});
+
+// Activate
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    })
+  );
+});
